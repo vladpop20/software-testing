20d5cc9f3368630ba35896fc20429c63cca86a77
Vlad-PC <vladpop20@gmail.com>
2023-03-26 22:20:59 +0300

    create a testing example

--- src/test/java/com/amigoscode/testing/customer/CustomerRepositoryTest.java
+++ src/test/java/com/amigoscode/testing/customer/CustomerRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.amigoscode.testing.customer;
 
+//import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 class CustomerRepositoryTest {
@@ -17,5 +22,25 @@ class CustomerRepositoryTest {
         // Given
         // When
         // Then  
+    }
+
+    @Test
+    public void itShouldSaveCustomer() {
+        // Given
+        UUID id = UUID.randomUUID();
+        Customer customerAdrian = new Customer(id, "Adrian", "0778901234");
+
+        // When
+        underTest.save(customerAdrian);
+
+        // Then
+        Optional<Customer> optionalCustomer = underTest.findById(id);
+        assertThat(optionalCustomer).isPresent()
+                .hasValueSatisfying(c -> {
+//                    assertThat(c.getId()).isEqualByComparingTo(id);
+//                    assertThat(c.getName()).isEqualTo("Adrian");
+//                    assertThat(c.getPhoneNumber()).isEqualTo("0778901234");
+                    assertThat(c).usingRecursiveComparison().isEqualTo(customerAdrian);
+                });
     }
 }
